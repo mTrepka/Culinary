8777620054faa07b61c83487125a87ed5e4f0914
qurppy <qurppy@gmail.com>
2019-03-06 12:03:57 +0100

    Initial commit

---
+++ src/main/java/com/mtrepka/culinary/CulinaryApplication.java
@@ -0,0 +1,13 @@
+package com.mtrepka.culinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryApplication.class, args);
+    }
+
+}
